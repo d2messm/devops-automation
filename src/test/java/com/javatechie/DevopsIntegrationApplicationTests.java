d11e7231128e3cbb65e586269884c4b9d463374d
d2messm <33947991+d2messm@users.noreply.github.com>
2022-11-09 19:58:47 +0530

    Update DevopsIntegrationApplicationTests.java

--- src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
+++ src/test/java/com/javatechie/DevopsIntegrationApplicationTests.java
@@ -1,13 +1,21 @@
-package com.javatechie;
+package hello;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class DevopsIntegrationApplicationTests {
+@SpringBootApplication
+@RestController
+public class Application {
 
-	@Test
-	void contextLoads() {
-	}
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
